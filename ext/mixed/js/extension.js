3b40da80e3685c304fdc740cfa7771a97c200729
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2019-09-28 23:15:04 -0400

    Merge timing

--- ext/mixed/js/extension.js
+++ ext/mixed/js/extension.js
@@ -62,3 +62,86 @@ if (EXTENSION_IS_BROWSER_EDGE) {
     // Edge does not have chrome defined.
     chrome = browser;
 }
+
+
+class Timer {
+    constructor() {
+        this.samples = [];
+        this.parent = null;
+    }
+
+    sample(name) {
+        const time = performance.now();
+        this.samples.push({
+            name,
+            time,
+            children: []
+        });
+    }
+
+    complete(skip) {
+        this.sample('complete');
+
+        Timer.current = this.parent;
+        if (this.parent === null) {
+            if (!skip) {
+                console.log(this.toString());
+            }
+        } else {
+            if (skip) {
+                const sample = this.parent.samples[this.parent.samples.length - 1];
+                sample.children.splice(sample.children.length - 1, 1);
+            }
+        }
+    }
+
+    duration(sampleIndex) {
+        const sampleIndexIsValid = (typeof sampleIndex === 'number');
+        const startIndex = (sampleIndexIsValid ? sampleIndex : 0);
+        const endIndex = (sampleIndexIsValid ? sampleIndex + 1 : this.times.length - 1);
+        return (this.times[endIndex].time - this.times[startIndex].time);
+    }
+
+    toString() {
+        const indent = '  ';
+        const name = this.samples[0].name;
+        const duration = this.samples[this.samples.length - 1].time - this.samples[0].time;
+        const extensionName = chrome.runtime.getManifest().name;
+        return `${name} took ${duration.toFixed(8)}ms  [${extensionName}]` + Timer.indent(this.getSampleString(), indent);
+    }
+
+    getSampleString() {
+        const indent = '  ';
+        const duration = this.samples[this.samples.length - 1].time - this.samples[0].time;
+        let message = '';
+
+        for (let i = 0, ii = this.samples.length - 1; i < ii; ++i) {
+            const sample = this.samples[i];
+            const sampleDuration = this.samples[i + 1].time - sample.time;
+            message += `\nSample[${i}] took ${sampleDuration.toFixed(8)}ms (${((sampleDuration / duration) * 100.0).toFixed(1)}%)  [${sample.name}]`;
+            for (const child of sample.children) {
+                message += Timer.indent(child.getSampleString(), indent);
+            }
+        }
+
+        return message;
+    }
+
+    static create(name) {
+        const t = new Timer();
+        t.sample(name);
+        const current = Timer.current;
+        if (current !== null) {
+            current.samples[current.samples.length - 1].children.push(t);
+            t.parent = current;
+        }
+        Timer.current = t;
+        return t;
+    }
+
+    static indent(message, indent) {
+        return message.replace(/\n/g, `\n${indent}`);
+    }
+}
+
+Timer.current = null;
