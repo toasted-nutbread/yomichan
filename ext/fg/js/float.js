8bd783ef8e200d3d35f79c84ce292039a420fd8e
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2020-07-03 13:39:39 -0400

    Organize window message handlers

--- ext/fg/js/float.js
+++ ext/fg/js/float.js
@@ -31,13 +31,13 @@ class DisplayFloat extends Display {
         this._token = null;
         this._nestedPopupsPrepared = false;
         this._windowMessageHandlers = new Map([
-            ['initialize',         {handler: this._initialize.bind(this), authenticate: false}],
-            ['configure',          {handler: this._configure.bind(this)}],
-            ['setOptionsContext',  {handler: ({optionsContext}) => this.setOptionsContext(optionsContext)}],
-            ['setContent',         {handler: ({type, details}) => this.setContent(type, details)}],
-            ['clearAutoPlayTimer', {handler: () => this._clearAutoPlayTimer()}],
-            ['setCustomCss',       {handler: ({css}) => this.setCustomCss(css)}],
-            ['setContentScale',    {handler: ({scale}) => this._setContentScale(scale)}]
+            ['initialize',         {handler: this._onMessageInitialize.bind(this), authenticate: false}],
+            ['configure',          {handler: this._onMessageConfigure.bind(this)}],
+            ['setOptionsContext',  {handler: this._onMessageSetOptionsContext.bind(this)}],
+            ['setContent',         {handler: this._onMessageSetContent.bind(this)}],
+            ['clearAutoPlayTimer', {handler: this._onMessageClearAutoPlayTimer.bind(this)}],
+            ['setCustomCss',       {handler: this._onMessageSetCustomCss.bind(this)}],
+            ['setContentScale',    {handler: this._onMessageSetContentScale.bind(this)}]
         ]);
 
         this.setOnKeyDownHandlers([
@@ -100,7 +100,7 @@ class DisplayFloat extends Display {
         this._autoPlayAudioTimer = window.setTimeout(() => super.autoPlayAudio(), 400);
     }
 
-    // Private
+    // Message handling
 
     _onMessage(e) {
         const data = e.data;
@@ -129,6 +129,48 @@ class DisplayFloat extends Display {
         const handler = handlerInfo.handler;
         handler(data.params);
     }
+
+    _onMessageInitialize(params) {
+        this._initialize(params);
+    }
+
+    async _onMessageConfigure({messageId, frameId, popupId, optionsContext, childrenSupported, scale}) {
+        this.setOptionsContext(optionsContext);
+
+        await this.updateOptions();
+
+        if (childrenSupported && !this._nestedPopupsPrepared) {
+            const {depth, url} = optionsContext;
+            this._prepareNestedPopups(popupId, depth, frameId, url);
+            this._nestedPopupsPrepared = true;
+        }
+
+        this._setContentScale(scale);
+
+        api.sendMessageToFrame(frameId, 'popupConfigured', {messageId});
+    }
+
+    _onMessageSetOptionsContext({optionsContext}) {
+        this.setOptionsContext(optionsContext);
+    }
+
+    _onMessageSetContent({type, details}) {
+        this.setContent(type, details);
+    }
+
+    _onMessageClearAutoPlayTimer() {
+        this._clearAutoPlayTimer.bind(this);
+    }
+
+    _onMessageSetCustomCss({css}) {
+        this.setCustomCss(css);
+    }
+
+    _onMessageSetContentScale({scale}) {
+        this._setContentScale(scale);
+    }
+
+    // Private
 
     _copySelection() {
         window.parent.postMessage('selectionCopy', '*');
@@ -162,22 +204,6 @@ class DisplayFloat extends Display {
         this._token = token;
 
         api.sendMessageToFrame(frameId, 'popupInitialized', {secret, token});
-    }
-
-    async _configure({messageId, frameId, popupId, optionsContext, childrenSupported, scale}) {
-        this.setOptionsContext(optionsContext);
-
-        await this.updateOptions();
-
-        if (childrenSupported && !this._nestedPopupsPrepared) {
-            const {depth, url} = optionsContext;
-            this._prepareNestedPopups(popupId, depth, frameId, url);
-            this._nestedPopupsPrepared = true;
-        }
-
-        this._setContentScale(scale);
-
-        api.sendMessageToFrame(frameId, 'popupConfigured', {messageId});
     }
 
     _isMessageAuthenticated(message) {
