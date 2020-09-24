459b922218edcfb427faba1657f56d9f448f5226
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2020-09-23 21:50:10 -0400

    Add getFileExtensionFromImageMediaType to MediaUtility

--- ext/bg/js/media-utility.js
+++ ext/bg/js/media-utility.js
@@ -67,4 +67,35 @@ class MediaUtility {
                 return null;
         }
     }
+
+    /**
+     * Gets the file extension for a corresponding media type.
+     * @param mediaType The media type to use.
+     * @returns A file extension including the dot for the media type,
+     *   otherwise null.
+     */
+    getFileExtensionFromImageMediaType(mediaType) {
+        switch (mediaType) {
+            case 'image/apng':
+                return '.apng';
+            case 'image/bmp':
+                return '.bmp';
+            case 'image/gif':
+                return '.gif';
+            case 'image/x-icon':
+                return '.ico';
+            case 'image/jpeg':
+                return '.jpeg';
+            case 'image/png':
+                return '.png';
+            case 'image/svg+xml':
+                return '.svg';
+            case 'image/tiff':
+                return '.tiff';
+            case 'image/webp':
+                return '.webp';
+            default:
+                return null;
+        }
+    }
 }
