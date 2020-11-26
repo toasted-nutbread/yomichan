eb256ea69e7e7096aa80018c238f20ed595a93cb
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2020-11-26 14:11:18 -0500

    Move client.css to popup-outer.css in /mixed

--- ext/fg/js/popup.js
+++ ext/fg/js/popup.js
@@ -334,7 +334,7 @@ class Popup extends EventDispatcher {
             useWebExtensionApi = false;
             parentNode = this._shadow;
         }
-        await dynamicLoader.loadStyle('yomichan-popup-outer-stylesheet', fileType, '/fg/css/client.css', useWebExtensionApi, parentNode);
+        await dynamicLoader.loadStyle('yomichan-popup-outer-stylesheet', fileType, '/mixed/css/popup-outer.css', useWebExtensionApi, parentNode);
     }
 
     _observeFullscreen(observe) {
