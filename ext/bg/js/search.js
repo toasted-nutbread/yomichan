62dbb816131861005cd6a9927f93b51afe327c1b
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2020-11-29 12:40:09 -0500

    Use JapaneseUtil as a class which is manually instantiated

--- ext/bg/js/search.js
+++ ext/bg/js/search.js
@@ -24,8 +24,8 @@
  */
 
 class DisplaySearch extends Display {
-    constructor() {
-        super('search');
+    constructor(japaneseUtil) {
+        super('search', japaneseUtil);
         this._searchButton = document.querySelector('#search-button');
         this._queryInput = document.querySelector('#search-textbox');
         this._introElement = document.querySelector('#intro');
@@ -38,6 +38,7 @@ class DisplaySearch extends Display {
         this._introAnimationTimer = null;
         this._clipboardMonitorEnabled = false;
         this._clipboardMonitor = new ClipboardMonitor({
+            japaneseUtil,
             clipboardReader: {
                 getText: async () => (await api.clipboardGet())
             }
