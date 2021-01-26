7e7695d24f76c282fc2247fde415401901b83e37
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2021-01-25 22:15:53 -0500

    Don't resize the search box unless it's necessary

--- ext/bg/js/search.js
+++ ext/bg/js/search.js
@@ -142,8 +142,10 @@ class DisplaySearch extends Display {
 
         if (typeof source !== 'string') { source = ''; }
 
-        this._queryInput.value = source;
-        this._updateSearchHeight();
+        if (this._queryInput.value !== source) {
+            this._queryInput.value = source;
+            this._updateSearchHeight();
+        }
         this._setIntroVisible(!valid, animate);
     }
 
