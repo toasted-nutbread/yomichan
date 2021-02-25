f4dc26a93fdafdbc9c7f125e0c40fb598d0fa9af
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2021-02-24 20:18:28 -0500

    Add AnkiUtil

---
+++ ext/js/data/anki-util.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2021  Yomichan Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This class has some general utility functions for working with Anki data.
+ */
+class AnkiUtil {
+    /**
+     * Gets the root deck name of a full deck name. If the deck is a root deck,
+     * the same name is returned. Nested decks are separated using '::'.
+     * @param deckName A string of the deck name.
+     * @returns A string corresponding to the name of the root deck.
+     */
+    static getRootDeckName(deckName) {
+        const index = deckName.indexOf('::');
+        return index >= 0 ? deckName.substring(0, index) : deckName;
+    }
+}
