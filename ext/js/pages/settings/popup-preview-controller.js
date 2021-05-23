d9501d2547a875aadc3822babc5acc88d205ce00
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2021-05-23 13:34:57 -0400

    Remove unused global

--- ext/js/pages/settings/popup-preview-controller.js
+++ ext/js/pages/settings/popup-preview-controller.js
@@ -15,10 +15,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/* global
- * wanakana
- */
-
 class PopupPreviewController {
     constructor(settingsController) {
         this._settingsController = settingsController;
